f3e44616303a36b25c5acb73b38508eab6d1f03a
nattesharan <nattesharan@gmail.com>
2024-04-27 17:02:16 +0530

    Topological sort using Kahns algorithm

--- graphs/Topological sort/solution.py
+++ graphs/Topological sort/solution.py
@@ -2,6 +2,29 @@ from collections import defaultdict
 
 class Solution:
     
+    def topogical_sort_kahns_algorithm(self, V, adj):
+        # contruct indegree dict
+        indegree = defaultdict(int)
+        for u in range(V):
+            for v in adj[u]:
+                indegree[v] += 1
+        # populate default queue
+        queue = []
+        for u in range(V):
+            if not indegree[u]:
+                queue.append(u)
+        
+        result = []
+        while queue:
+            u = queue.pop(0)
+            result.append(u)
+            for v in adj[u]:
+                indegree[v] -= 1
+                # when indegree is 0 push to queue
+                if not indegree[v]:
+                    queue.append(v)
+        return result
+    
     def topolgical_sort_dfs(self, u, visited, adj_list, stack):
         visited.add(u)
         for v in adj_list[u]:
@@ -11,7 +34,9 @@ class Solution:
         stack.append(u)
     
     #Function to return list containing vertices in Topological order.
-    def topoSort(self, V, adj):
+    def topoSort(self, V, adj, use_kahns_algorithm=True):
+        if use_kahns_algorithm:
+            return self.topogical_sort_kahns_algorithm(V, adj)
         visited = set()
         adj_list = defaultdict(list)
         stack = []
