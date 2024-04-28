52451c966699b30704a7f8ceeabc7c8bbb9e75db
nattesharan <nattesharan@gmail.com>
2024-04-28 15:15:24 +0530

    Course schedule using DFS

--- graphs/Course Schedule/solution.py
+++ graphs/Course Schedule/solution.py
@@ -1,7 +1,8 @@
 from typing import List
 from collections import defaultdict
 
-class Solution:
+# simple cycle detection in directed graph
+class SolutionBFS:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj_list = defaultdict(list)
         indegree = defaultdict(int)
@@ -26,8 +27,32 @@ class Solution:
             return True
         return False
 
+class SolutionDFS:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        def dfs_cycle_exists(u, adj_list, in_recursion, visited):
+            visited.add(u)
+            in_recursion.add(u)
+            for v in adj_list[u]:
+                if v not in visited and dfs_cycle_exists(v, adj_list, in_recursion, visited):
+                    return True
+                elif v in in_recursion:
+                    return True
+            in_recursion.remove(u)
+            return False
+
+        adj_list = defaultdict(list)
+        for prerequisite in prerequisites:
+            a, b = prerequisite
+            adj_list[b].append(a)
+        visited = set()
+        in_recursion = set()
+        for i in range(numCourses):
+            if i not in visited and dfs_cycle_exists(i, adj_list, in_recursion, visited):
+                return False
+        return True
+
 if __name__ == '__main__':
-    solution = Solution()
+    solution = SolutionDFS()
     numCourses = 2
     prerequisites = [[1,0],[0,1]]
     print(solution.canFinish(numCourses, prerequisites))
